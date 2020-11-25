76680f209ad4acfc97faf6081a0d2bb3fa138c31
Alankrit Sinha <60623913+alankritOP@users.noreply.github.com>
2020-11-25 10:32:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import App from './App';
-//import './index.css';
-//import reportWebVitals from './reportWebVitals';*/
-
-
-
-///*function formatUser(user){
- //  return user.firstName+" "+user.lastName;
-//}
-
-//const user={
-  //firstName:"alankrit",
-  //lastName:"Sinha"
-//};
-
-//const element=
-//(
-  //<h1>
-  //  Hello from {formatUser(user)}
-//</h1>
-//);
-//*/
-///*ReactDOM.render(
-  //<App/>,
-  //document.getElementById('root')
-//);*/
-
 import React from "react";
 import ReactDOM from "react-dom";
 
